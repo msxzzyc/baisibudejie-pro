ea3e2e4b2acae1c63b515e0090f8d772ef4966b3
msxzzyc <10168363742qq.com>
2017-09-06 00:04:20 +0800

    基本配置

---
+++ baisibudejie-pro/baisibudejie-pro/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  baisibudejie-pro
+//
+//  Created by wpzyc on 2017/9/5.
+//  Copyright © 2017年 wpzyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ baisibudejie-pro/baisibudejie-pro/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  baisibudejie-pro
+//
+//  Created by wpzyc on 2017/9/5.
+//  Copyright © 2017年 wpzyc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
