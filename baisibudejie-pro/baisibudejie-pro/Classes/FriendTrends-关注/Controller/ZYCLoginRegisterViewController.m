500f7bc862365aa9b4f2ff740438e5ffd713d688
msxzzyc <10168363742qq.com>
2017-10-22 22:39:26 +0800

    设置注册框

--- baisibudejie-pro/baisibudejie-pro/Classes/FriendTrends-关注/Controller/ZYCLoginRegisterViewController.m
+++ baisibudejie-pro/baisibudejie-pro/Classes/FriendTrends-关注/Controller/ZYCLoginRegisterViewController.m
@@ -10,10 +10,33 @@
 
 @interface ZYCLoginRegisterViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *phoneField;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginViewLeftMargin;
 
 @end
 
 @implementation ZYCLoginRegisterViewController
+- (IBAction)back:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)showLoginOrRegister:(UIButton *)button {
+    [self.view endEditing:YES];
+    
+    if (self.loginViewLeftMargin.constant == 0) {
+//        button.selected = YES;
+        [button setTitle:@"已有账号？" forState:UIControlStateNormal];
+        self.loginViewLeftMargin.constant = -self.view.width;
+    } else {
+                button.selected = NO ;
+        [button setTitle:@"注册账号" forState:UIControlStateNormal];
+        self.loginViewLeftMargin.constant = 0;
+    }
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+    
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
