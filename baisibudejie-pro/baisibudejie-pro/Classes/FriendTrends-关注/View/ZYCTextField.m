54223034d7cfc4555b07c538432ed2554691ef4c
msxzzyc <10168363742qq.com>
2017-10-22 21:45:27 +0800

    使用runtime及kvc改变占位文字颜色

--- baisibudejie-pro/baisibudejie-pro/Classes/FriendTrends-关注/View/ZYCTextField.m
+++ baisibudejie-pro/baisibudejie-pro/Classes/FriendTrends-关注/View/ZYCTextField.m
@@ -7,8 +7,23 @@
 //
 
 #import "ZYCTextField.h"
+#import <objc/runtime.h>
 
+static NSString *const ZYCPlaceholderColorKeyPath = @"_placeholderLabel.textColor";
 @implementation ZYCTextField
+
+//+ (void)initialize
+//{
+//    unsigned int count = 0;
+//    Ivar *ivars = class_copyIvarList([UITextField class], &count);
+//    for (int i = 0; i<count; i++) {
+//        Ivar ivar = *(ivars + i);
+//        Ivar ivar = ivars[i];
+//        ZYCLog(@"%s",ivar_getName(ivar));
+//    }
+//    free(ivars);
+//
+//}
 
 /*
 // Only override drawRect: if you perform custom drawing.
@@ -17,9 +32,33 @@
     // Drawing code
 }
 */
-- (void)drawPlaceholderInRect:(CGRect)rect
+
+- (void)awakeFromNib
 {
-//    [super drawPlaceholderInRect:rect];
-    [self.placeholder drawInRect:CGRectMake(0, 3, 100, 25) withAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor],NSFontAttributeName : self.font}];
+    [super awakeFromNib];
+    
+//    UILabel *placeholderLabel = [self valueForKeyPath:@"_placeholderLabel"];
+//    placeholderLabel.textColor = [UIColor whiteColor];
+//    [self setValue:[UIColor grayColor] forKeyPath:@"_placeholderLabel.textColor"];
+    [self resignFirstResponder];
+    self.tintColor = self.textColor;
 }
+
+- (BOOL)becomeFirstResponder
+{
+    [self setValue:self.textColor forKeyPath:ZYCPlaceholderColorKeyPath];
+    return [super becomeFirstResponder];
+    
+}
+
+- (BOOL)resignFirstResponder
+{
+    [self setValue:[UIColor grayColor] forKeyPath:ZYCPlaceholderColorKeyPath];
+    return [super resignFirstResponder];
+}
+//- (void)drawPlaceholderInRect:(CGRect)rect
+//{
+////    [super drawPlaceholderInRect:rect];
+//    [self.placeholder drawInRect:CGRectMake(0, 3, 100, 25) withAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor],NSFontAttributeName : self.font}];
+//}
 @end
