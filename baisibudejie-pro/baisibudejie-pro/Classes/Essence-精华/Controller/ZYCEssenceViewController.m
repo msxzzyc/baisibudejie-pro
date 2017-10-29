00044d6708a8b837cdc112061b0e425381016af6
msxzzyc <10168363742qq.com>
2017-10-29 20:54:06 +0800

    顶部标签内容

--- baisibudejie-pro/baisibudejie-pro/Classes/Essence-精华/Controller/ZYCEssenceViewController.m
+++ baisibudejie-pro/baisibudejie-pro/Classes/Essence-精华/Controller/ZYCEssenceViewController.m
@@ -9,7 +9,7 @@
 #import "ZYCEssenceViewController.h"
 #import "ZYCRecommendTagsViewController.h"
 @interface ZYCEssenceViewController ()
-
+@property(weak,nonatomic)UIView *indicator;
 @end
 
 @implementation ZYCEssenceViewController
@@ -17,14 +17,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.navigationItem.titleView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"MainTitle"]];
-    
-    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithimage:@"MainTagSubIcon" highImage:@"MainTagSubIconClick" target:self action:@selector(essencebuttonClick)];
+    [self setNav];
+    [self setTitleView];
     
     self.view.backgroundColor = ZYCGlobalBg;
 
 }
-
+- (void)setTitleView
+{
+    UIView *titleView = [[UIView alloc]init];
+    titleView.width = self.view.width;
+    titleView.height = 35;
+    titleView.y = 64;
+    
+    titleView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7];
+    
+    [self.view addSubview:titleView];
+    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
+    NSInteger count = 5;
+    
+    CGFloat width = titleView.width/titles.count;
+    CGFloat height = titleView.height;
+    for (NSInteger i = 0; i<count; i++) {
+        UIButton *button = [[UIButton alloc]init];
+        button.width = width;
+        button.height = height;
+        button.x = i*width;
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+        [titleView addSubview:button];
+    }
+    UIView *indicator = [[UIView alloc]init];
+    indicator.backgroundColor = [UIColor redColor];
+    indicator.height = 2;
+    indicator.y = titleView.height - indicator.height;
+    [titleView addSubview:indicator];
+    self.indicator = indicator;
+}
+- (void)titleClick:(UIButton *)button
+{
+    [UIView animateWithDuration:0.25 animations:^{
+        self.indicator.centerX = button.centerX;
+        self.indicator.width = button.titleLabel.width;
+    }];
+    
+    ZYCLogFuc;
+}
+- (void)setNav
+{
+    self.navigationItem.titleView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"MainTitle"]];
+    
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithimage:@"MainTagSubIcon" highImage:@"MainTagSubIconClick" target:self action:@selector(essencebuttonClick)];
+}
 - (void)essencebuttonClick
 {
     ZYCRecommendTagsViewController *tagsVc = [[ZYCRecommendTagsViewController alloc]init];
--- baisibudejie-pro/baisibudejie-pro/Classes/Other-其它/Category/UIView+ZYCExtention.h
+++ baisibudejie-pro/baisibudejie-pro/Classes/Other-其它/Category/UIView+ZYCExtention.h
@@ -15,6 +15,8 @@
 @property(nonatomic,assign)CGFloat x;
 @property(nonatomic,assign)CGFloat y;
 
+@property(nonatomic,assign)CGFloat centerX;
+@property(nonatomic,assign)CGFloat centerY;
 
 
 @end
