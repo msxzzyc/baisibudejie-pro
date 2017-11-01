9a044fa2ca873237ff2456978a719fc8b7c75cda
msxzzyc <10168363742qq.com>
2017-11-01 21:51:50 +0800

    显示子控制器

--- baisibudejie-pro/baisibudejie-pro/Classes/Essence-精华/Controller/ZYCEssenceViewController.m
+++ baisibudejie-pro/baisibudejie-pro/Classes/Essence-精华/Controller/ZYCEssenceViewController.m
@@ -8,8 +8,16 @@
 
 #import "ZYCEssenceViewController.h"
 #import "ZYCRecommendTagsViewController.h"
-@interface ZYCEssenceViewController ()
+#import "ZYCAllViewController.h"
+#import "ZYCVideoViewController.h"
+#import "ZYCVoiceViewController.h"
+#import "ZYCPictureViewController.h"
+#import "ZYCWordViewController.h"
+@interface ZYCEssenceViewController ()<UIScrollViewDelegate>
 @property(weak,nonatomic)UIView *indicator;
+@property(weak,nonatomic)UIButton *selectedButton;
+@property(weak,nonatomic)UIView *titleView;
+@property(weak,nonatomic)UIScrollView *contentView;
 @end
 
 @implementation ZYCEssenceViewController
@@ -17,11 +25,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    
     [self setNav];
+    [self setUpChildVces];
+    
     [self setTitleView];
+    
+    [self setContentView];
+    
     
     self.view.backgroundColor = ZYCGlobalBg;
 
+    
 }
 - (void)setTitleView
 {
@@ -31,38 +46,94 @@
     titleView.y = 64;
     
     titleView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7];
-    
+    self.titleView = titleView;
     [self.view addSubview:titleView];
-    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
-    NSInteger count = 5;
+    UIView *indicator = [[UIView alloc]init];
+    indicator.backgroundColor = [UIColor redColor];
+    indicator.height = 2;
+    indicator.y = titleView.height - indicator.height;
     
+    self.indicator = indicator;
+    
+    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
     CGFloat width = titleView.width/titles.count;
     CGFloat height = titleView.height;
-    for (NSInteger i = 0; i<count; i++) {
+    for (NSInteger i = 0; i<titles.count; i++) {
         UIButton *button = [[UIButton alloc]init];
         button.width = width;
         button.height = height;
         button.x = i*width;
         [button setTitle:titles[i] forState:UIControlStateNormal];
+        button.tag = i;
+        [button layoutIfNeeded];
         [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor redColor] forState:UIControlStateDisabled];
+        button.titleLabel.font = [UIFont systemFontOfSize:14];
         [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
         [titleView addSubview:button];
+        
+        if ( i == 0) {
+            button.enabled = NO;
+            self.selectedButton = button;
+//            [button.titleLabel sizeToFit];
+            
+//            self.indicator.width = button.titleLabel.width;
+            self.indicator.width = [titles[i] sizeWithAttributes:@{NSFontAttributeName : button.titleLabel.font}].width;
+            self.indicator.centerX = button.centerX;
+            
+        }
+        [titleView addSubview:indicator];
     }
-    UIView *indicator = [[UIView alloc]init];
-    indicator.backgroundColor = [UIColor redColor];
-    indicator.height = 2;
-    indicator.y = titleView.height - indicator.height;
-    [titleView addSubview:indicator];
-    self.indicator = indicator;
+    
+    
 }
 - (void)titleClick:(UIButton *)button
 {
+    self.selectedButton.enabled = YES;
+    button.enabled = NO;
+    self.selectedButton = button;
     [UIView animateWithDuration:0.25 animations:^{
         self.indicator.centerX = button.centerX;
         self.indicator.width = button.titleLabel.width;
+        
     }];
-    
+    CGPoint offSet = self.contentView.contentOffset;
+    offSet.x = button.tag *self.contentView.width;
+    [self.contentView setContentOffset:offSet animated:YES];
     ZYCLogFuc;
+}
+
+- (void)setContentView
+{
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    UIScrollView *contentView = [[UIScrollView alloc]init];
+    contentView.frame = self.view.bounds;
+    contentView.delegate = self;
+    contentView.pagingEnabled = YES;
+
+    [self.view insertSubview:contentView atIndex:0];
+    self.contentView = contentView;
+    self.contentView.contentSize = CGSizeMake(contentView.width *self.childViewControllers.count, 0);
+    [self scrollViewDidEndScrollingAnimation:contentView];
+}
+- (void)setUpChildVces
+{
+    ZYCAllViewController *all = [[ZYCAllViewController alloc]init];
+    [self addChildViewController:all];
+    
+    ZYCVideoViewController *video = [[ZYCVideoViewController alloc]init];
+    [self addChildViewController:video];
+    
+    ZYCVoiceViewController *voice = [[ZYCVoiceViewController alloc]init];
+    [self addChildViewController:voice];
+    
+    ZYCPictureViewController *picture = [[ZYCPictureViewController alloc]init];
+    [self addChildViewController:picture];
+    
+    ZYCWordViewController *word = [[ZYCWordViewController alloc]init];
+    [self addChildViewController:word];
+    
+    
 }
 - (void)setNav
 {
@@ -80,6 +151,29 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView
+{
+    NSInteger index = scrollView.contentOffset.x/scrollView.width;
+    
+    UITableViewController *vc = self.childViewControllers[index];
+    vc.tableView.x = scrollView.contentOffset.x;
+    CGFloat top = CGRectGetMaxY(self.titleView.frame);
+    CGFloat bottom = self.tabBarController.tabBar.height;
+    
+    vc.tableView.contentInset = UIEdgeInsetsMake(top, 0, bottom, 0);
+    [scrollView addSubview:vc.view];
+    
+    
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    [self scrollViewDidEndScrollingAnimation:scrollView];
+    
+    NSInteger index = scrollView.contentOffset.x/scrollView.width;
+    [self titleClick:self.titleView.subviews[index]];
+    
+}
 /*
 #pragma mark - Navigation
 
---
+++ baisibudejie-pro/baisibudejie-pro/Classes/Essence-精华/Controller/ZYCVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZYCVideoViewController.h
+//  baisibudejie-pro
+//
+//  Created by wpzyc on 2017/10/31.
+//  Copyright © 2017年 wpzyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYCVideoViewController : UITableViewController
+
+@end
